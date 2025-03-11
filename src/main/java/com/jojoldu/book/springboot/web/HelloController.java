7bbc8485fbabed1804e02a27d0e19b5913d818cb
hjlee <hjlee-kr@naver.com>
2025-03-11 10:00:56 +0900

    250311-1교시-3.helloDto()

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -7,7 +7,9 @@ package com.jojoldu.book.springboot.web;
 - @~~~Advice - 예외처리를 위한 클래스
 * */
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,4 +22,15 @@ public class HelloController {
         return "hello";
     }
     // @GetMapping
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(
+            @RequestParam("name") String name,
+            @RequestParam("amount") int amount
+    ) {
+        // @RequestParam 에 적힌 값은 get방식에서는
+        // 웹브라우저 주소창에 적힌 변수이름과 매칭됩니다.
+        // localhost:8080/hello/dto?name=a&amount=1
+        return new HelloResponseDto(name, amount);
+    }
 }
